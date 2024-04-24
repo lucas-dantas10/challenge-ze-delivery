a672c1ad0660fc9bdcee9f886492eda12441a5ea
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2024-04-24 17:03:10 -0300

    feat: prepare entities

---
+++ src/Entity/PartnerEntity/Partner.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Entity\PartnerEntity;
+
+use CrEOF\Spatial\PHP\Types\Geometry\Point;
+use CrEOF\Spatial\PHP\Types\Geometry\MultiPolygon;
+
+class Partner
+{
+    private int $id;
+    private string $tradingName;
+    private string $ownerName;
+    private string $document;
+    private $coverageArea;
+    private $address;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getTradingName(): string
+    {
+        return $this->tradingName;
+    }
+
+    public function setTradingName(string $tradingName): void
+    {
+        $this->tradingName = $tradingName;
+    }
+
+    public function getOwnerName(): string
+    {
+        return $this->ownerName;
+    }
+
+    public function setOwnerName(string $ownerName): void
+    {
+        $this->ownerName = $ownerName;
+    }
+
+    public function getDocument(): string
+    {
+        return $this->document;
+    }
+
+    public function setDocument(string $document): void
+    {
+        $this->document = $document;
+    }
+
+    public function getCoverageArea(): \GEOSGeometry
+    {
+        return $this->coverageArea;
+    }
+
+    public function setCoverageArea(MultiPolygon $coverageArea): void
+    {
+        $this->coverageArea = $coverageArea;
+    }
+
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    public function setAddress(Point $address): void
+    {
+        $this->address = $address;
+    }
+
+
+}
